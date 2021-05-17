347176f14350f9de8989d46af139d7fd019e0ed9
sammy <ehuerikenbaba@gmail.com>
2021-05-17 22:47:22 +0100

    added path

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require('path');
 
 require("dotenv").config();
 
@@ -11,18 +12,23 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-})
+});
 
 const exercisesRouter = require("./routes/exercises");
 const usersRouter = require("./routes/users");
 
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
+
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.static(buildPath));
+app.get("*", (req, res) => {
+  res.sendFile(buildPath);
+});
 
 app.listen(port, () => {
   console.log(`server is running on port:${port}`);
